0b68afb555796be2c29690dc9429b04975002f64
Kwok Chun Ho <kch062522@gmail.com>
2020-04-29 11:33:03 +0800

    fix: use process.env.port to define port

--- node-frontend/app.js
+++ node-frontend/app.js
@@ -1,13 +1,20 @@
 let express = require('express');
 let app = express();
 
-//AWS SQS
+//AWS
 let AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'}); // Set the region 
-let sqs = new AWS.SQS(); // Create an SQS service object
+
+// SQS
+let sqs = new AWS.SQS();
+
+//DynamoDB
+
+// Create the DynamoDB service object
+var ddb = new AWS.DynamoDB();
 
 // port to host
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // js & css
 app.use(express.static('public'));
@@ -26,6 +33,16 @@ app.get('/', (req, res) => {
 
 // POST /
 app.post('/addmember', (req, res) => {
+    queue(req);
+    insertToDB(req);
+    res.render('login');
+});
+
+app.listen(port, () => {
+    console.log(`Server start running on http://localhost:${port}/`);
+});
+
+function queue(req) {
     let params = {
         MessageBody: `Add new member: ${req.body.name}, ${req.body.email}, ${req.body.age}, ${req.body.sex}`,
         MessageAttributes: {
@@ -48,15 +65,32 @@ app.post('/addmember', (req, res) => {
         },
         QueueUrl: "https://sqs.us-east-1.amazonaws.com/300144314676/new_member"
     };
+
     sqs.sendMessage(params, (err, data) => {
         if (err)
             console.error(err);
         else
-            console.log("Add member successfully.", data.MessageId);
+            console.log("Add member successfully.", data);
     });
-    res.render('login');
-});
+}
 
-app.listen(port, () => {
-    console.log(`Server start running on http://localhost:${port}/`);
-});
+function insertToDB(req) {
+    let dbparams = {
+        TableName: 'user',
+        Item: {
+          'name' : {S: `${req.body.name}`},
+          'email' : {S: `${req.body.email}`},
+          'age' : {N: `${req.body.age}`},
+          'sex' : {S: `${req.body.sex}`}
+        }
+      };
+      
+      // Call DynamoDB to add the item to the table
+      ddb.putItem(dbparams, function(err, data) {
+        if (err) {
+          console.log("Error", err);
+        } else {
+          console.log("Insert into table successfully", data);
+        }
+      });
+}
--- node-frontend/public/js/main.js
+++ node-frontend/public/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
     $('form, div, button').each(function (i) {
-        $(this).delay(i * 500).animate({ opacity: 0.8 });
+        $(this).delay(i * 500).animate({ opacity: 0.9 });
     });
 });
